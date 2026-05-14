1873a2ed46b2d46a8c06588c531f2b5d767ea26f
MrShel12 <br0896198361@gmail.com>
2026-05-13 18:57:44 -0600

    avances

---
+++ saludmental/src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+
+  firebase: {
+
+    apiKey: "TU_API_KEY",
+    authDomain: "TU_AUTH_DOMAIN",
+    projectId: "TU_PROJECT_ID",
+    storageBucket: "TU_STORAGE_BUCKET",
+    messagingSenderId: "TU_SENDER_ID",
+    appId: "TU_APP_ID"
+
+  }
+
+};
